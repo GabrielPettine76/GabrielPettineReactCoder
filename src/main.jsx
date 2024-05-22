12bb51333067da47d27f707623d38cf8ece91270
Gabriel Pettine <gabriel_pettine@yahoo.com>
2024-05-21 22:16:56 -0300

    solucionar error

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { initializeApp } from 'firebase/app';
 import firebaseConfig from './FirebaseConfig.jsx';
-import firebaseConfig from './FirebaseConfig.jsx';
-const firebaseConfig = firebaseConfig;
 
-// Initialize Firebase
 initializeApp(firebaseConfig);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
